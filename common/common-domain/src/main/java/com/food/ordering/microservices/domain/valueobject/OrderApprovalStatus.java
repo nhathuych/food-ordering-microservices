2887cd635b711e8c3c8a8ef68e8a3a25692415fb
nhathuych <nhathuych@gmail.com>
2026-04-17 21:03:57 +0700

    implement Order Application Service - DTO classes

---
+++ common/common-domain/src/main/java/com/food/ordering/microservices/domain/valueobject/OrderApprovalStatus.java
@@ -0,0 +1,5 @@
+package com.food.ordering.microservices.domain.valueobject;
+
+public enum OrderApprovalStatus {
+    APPROVED, REJECTED
+}
---
+++ common/common-domain/src/main/java/com/food/ordering/microservices/domain/valueobject/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.food.ordering.microservices.domain.valueobject;
+
+public enum PaymentStatus {
+    COMPLETED, CANCELLED, FAILED
+}
