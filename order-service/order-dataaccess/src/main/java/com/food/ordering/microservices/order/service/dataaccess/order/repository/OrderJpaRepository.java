c828f974318e5371573b5fcbaf8fd109877a1c4c
nhathuych <nhathuych@gmail.com>
2026-04-24 19:20:16 +0700

    add JPA Repository & Adapter implementation

--- order-service/order-dataaccess/src/main/java/com/food/ordering/microservices/order/service/dataaccess/order/repository/OrderJpaRepository.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/microservices/order/service/dataaccess/order/repository/OrderJpaRepository.java
@@ -1,4 +1,13 @@
 package com.food.ordering.microservices.order.service.dataaccess.order.repository;
 
-public interface OrderJpaRepository {
+import com.food.ordering.microservices.order.service.dataaccess.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderJpaRepository extends JpaRepository<OrderEntity, UUID> {
+    Optional<OrderEntity> findByTrackingId(UUID trackingId);
 }
