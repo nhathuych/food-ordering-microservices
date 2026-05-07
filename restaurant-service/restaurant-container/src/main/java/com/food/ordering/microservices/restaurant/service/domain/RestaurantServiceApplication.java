45d5209cab7aabf3119fdc07b5415fb1e804a3c0
nhathuych <nhathuych@gmail.com>
2026-05-07 19:48:27 +0700

    fix package scanning configuration

--- restaurant-service/restaurant-container/src/main/java/com/food/ordering/microservices/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/microservices/restaurant/service/domain/RestaurantServiceApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = { "com.food.ordering.system.restaurant.service.dataaccess", "com.food.ordering.system.dataaccess" })
-@EntityScan(basePackages = { "com.food.ordering.system.restaurant.service.dataaccess", "com.food.ordering.system.dataaccess" })
-@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+@EnableJpaRepositories(basePackages = {
+        "com.food.ordering.microservices.restaurant.service.dataaccess",
+        "com.food.ordering.microservices.dataaccess"
+})
+@EntityScan(basePackages = {
+        "com.food.ordering.microservices.restaurant.service.dataaccess",
+        "com.food.ordering.microservices.dataaccess"
+})
+@SpringBootApplication(scanBasePackages = "com.food.ordering.microservices")
 public class RestaurantServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestaurantServiceApplication.class, args);
