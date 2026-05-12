49854c0fc2815b5b287c4898206c049947914519
nhathuych <nhathuych@gmail.com>
2026-05-12 20:07:20 +0700

    refactor Order domain layer: add Outbox scheduler

---
+++ infrastructure/saga/src/main/java/com/food/ordering/microservices/saga/order/SagaConstants.java
@@ -0,0 +1,8 @@
+package com.food.ordering.microservices.saga.order;
+
+public final class SagaConstants {
+    public static final String ORDER_SAGA_NAME = "OrderProcessingSaga";
+
+    private SagaConstants() {
+    }
+}
